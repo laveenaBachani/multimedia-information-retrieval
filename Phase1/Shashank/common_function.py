9bf16a9897bad5d053d04efaa3a3ccf85f34e390
shanky <shashank.prof.05@gmail.com>
2018-09-07 02:01:00 -0700

    Done with first 3 tasks

---
+++ Phase1/Shashank/common_function.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+from math import sqrt
+
+
+def fetch_data(file_name):
+    """
+    :return: a counter for user, this will be a vector kind of concept
+    """
+    d = {}
+    for user in open('../{0}/{1}'.format('Data', file_name)).readlines():
+        user = user.split(' ')
+        user_id = user.pop(0)
+        d[user_id] = defaultdict(lambda: {})
+        while len(user) > 1:
+            key, tf, df, tfidf = user.pop(0), float(user.pop(0)), float(user.pop(0)), float(user.pop(0))
+            d[user_id][key] = {'TF': tf, 'DF': df, 'TF-IDF': tfidf}
+    return d
+
+
+def cosine_similarity(u1, u2, model):
+    intersect = set(u1.keys()) & set(u2.keys())
+    numerator, denominator = sum([u1[x][model] * u2[x][model] for x in intersect]), sqrt(
+        sum([u1[x][model] ** 2 for x in u1])) * sqrt(sum([u2[x][model] ** 2 for x in u2]))
+    if not denominator:
+        return 0.0, []
+    else:
+        return float(numerator) / float(denominator), sorted([x for x in intersect],
+                                                             key=lambda x: (u1[x][model] * u2[x][model], x))[:3]
---
+++ Phase1/Shashank/task3.py
@@ -0,0 +1,29 @@
+import argparse
+from heapq import *
+from common_function import *
+
+
+def parse_args_process():
+    argument_parse = argparse.ArgumentParser()
+    argument_parse.add_argument('--location_id', help='The user you want to choose', type=str, required=True)
+    argument_parse.add_argument('--model', help='The model you want to use(TF/DF/TF-IDF)', type=str, required=True)
+    argument_parse.add_argument('--k', help='The number of neighbours you want to find', type=int, required=True)
+    args = argument_parse.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args_process()
+    user_data = fetch_data('devset_textTermsPerPOI.txt')
+    heap = []
+    for x in user_data:
+        if x != args.location_id:
+            similarity, top_3 = cosine_similarity(user_data[x], user_data[args.location_id], args.model)
+            heappush(heap, (similarity, x, top_3))
+            if len(heap) > args.k:
+                heappop(heap)
+
+    ans = []
+    while heap:
+        ans.append(heappop(heap))
+    print(ans[::-1])
