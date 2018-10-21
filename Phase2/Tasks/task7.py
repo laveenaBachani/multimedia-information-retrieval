a7131845228149a16823bbeb97b41772a11dfda9
ialba75 <ialba@asu.edu>
2018-10-21 09:29:26 -0700

    Finished task 7

--- Phase2/Tasks/task7.py
+++ Phase2/Tasks/task7.py
@@ -1,78 +1,136 @@
-# TODO:
-# - creates an user-image-location tensor (based on number of terms shared by all three)
-# - performs rank-k CP decomposition of this tensor
-# - creates k non-overlapping groups of users, images, and locations based on the discovered latent semantics.
-
 from APIs.generic_apis import *
 import xmltodict
 import numpy as np
-import operator
 import os
+from tensorly.decomposition import parafac
+from joblib import Parallel, delayed
+import multiprocessing
 
-def dotProduct(v1, v2):
-    return sum(map(operator.mul, v1, v2))
 
+# Load name for each locationId
 locations = []
 with open("../Data/devset_topics.xml") as fd:
     doc = xmltodict.parse(fd.read())
     for topic in doc['topics']['topic']:
         locations.append(topic['title'])
 
+
+def printGroups(groups, i, f):
+    """
+    Utility function for writing group information to file
+
+    :param groups: list of length k, each bucket holds member id's for kth-group
+    :param i: index of group to be printed
+    :param f: file handler
+    :return:
+    """
+    f.write("Group #" + str(i + 1) + " with " + str(len(groups[i])) + " members:\n")
+    f.write(' '.join(groups[i]) + '\n')
+
+
 def task7():
-    # k = int(input("Enter a value for k: "))
+    """
+    Core logic for task7
+    """
+    num_cores = multiprocessing.cpu_count()
+    k = int(input("Enter a value for k: "))
     tensorFileName = "userImageLocation-tensor.npy"
+    factorMatricesFileName = "factor-matrices.npy"
     cwd = os.getcwd()
 
-    print ("Loading User Space...")
+    # Load each object space into dictionary where d_obj = {'id': {'term': df, ...}, ...}
+    print("Loading User Space...")
     userFile = '../Data/devset_textTermsPerUser.txt'
-    userVector, userTexts, users = tDictionary_to_vector(read_text_descriptor_files(userFile))
-    users = list(users)
+    d_user = read_text_descriptor_files(userFile)
 
-    # print str(users.index("53074617@N00"))
-
-    print ("Loading Image Space...")
+    print("Loading Image Space...")
     imageFile = '../Data/devset_textTermsPerImage.txt'
-    imageVector, imageTexts, images = tDictionary_to_vector(read_text_descriptor_files(imageFile))
-    images = list(images)
+    d_images = read_text_descriptor_files(imageFile)
 
-    print ("Loading Location Space...")
+    print("Loading Location Space...")
     locFile = '../Data/devset_textTermsPerPOI.txt'
-    locVector, locTexts, locations = tDictionary_to_vector(read_text_descriptor_files(locFile))
-    locations = list(locations)
+    d_locations = read_text_descriptor_files(locFile)
+
+    user_list = list(d_user.keys())
+    image_list = list(d_images.keys())
+    loc_list = list(d_locations.keys())
+    print(len(user_list), len(image_list), len(loc_list))
 
     if os.path.exists(cwd + '/' + tensorFileName):
-        print ("Loading Tensor...")
+        print("Loading Tensor...")
         tensor = np.load(tensorFileName)
     else:
-        print ("Creating Tensor...")
-        tensor_shape = (len(users), len(images), len(locations)) # user, image, location
-        tensor = np.zeros(tensor_shape, dtype=int)
+        print("Creating Tensor...")
 
-        # Only interested in if term exists per vector, convert elements to only 0's and 1's
-        userVector[userVector > 0] = 1
-        imageVector[imageVector > 0] = 1
-        locVector[locVector > 0] = 1
+        def processInput(i):
+            # Create a slice of 3-D tensor (combinations of loc & image per user)
+            print('Started for user' + str(i))
+            user = user_list[i]
+            array = [[0 for _ in range(len(loc_list))] for _ in range(len(image_list))]
+            for j in range(len(image_list)):
+                for l in range(len(loc_list)):
+                    image = image_list[j]
+                    loc = loc_list[l]
+                    # Number of terms shared by all three entities
+                    union_words = d_user[user].keys() & d_images[image].keys() & d_locations[loc].keys()
+                    array[j][l] += len(union_words)
 
-        # User and Image number of shared terms
-        # userImageMatrix = userVector @ np.transpose(imageVector)
+            print('Ended for user' + str(i))
+            return array
 
-        # Brute force approach: get # of terms shared per user, image, location triplet
-        for locationIndex in range(len(locations)):
-            print ("Location... ", locations[locationIndex], " - ", str(locationIndex))
-            for imageIndex in range(len(images)):
-                if imageIndex % int(len(images) * 0.10) == 0:
-                    print (str(imageIndex / len(images) * 100))
-                for userIndex in range(len(users)):
-                    numSharedTerms = sum(userVector[userIndex] * imageVector[imageIndex] * locVector[locationIndex])
-                    tensor[userIndex][imageIndex][locationIndex] = numSharedTerms
-
+        tensor = Parallel(n_jobs=num_cores - 1)(delayed(processInput)(i) for i in range(len(user_list)))
+        tensor = np.array(tensor)
+        print(tensor.shape)
         np.save(tensorFileName, tensor)
 
-    print (tensor.shape)
-    print ("OI")
+    print('Tensor created')
 
+    if not os.path.exists(cwd + '/' + factorMatricesFileName):
+        # Perform CP decomposition via ALS
+        tensor = tensor.astype(float)
+        print("Performing CP Decomposition...")
+        factors = parafac(tensor=tensor, rank=k, n_iter_max=150, init='random')
+        np.save(factorMatricesFileName, factors)
+    else:
+        factors = np.load(factorMatricesFileName)
 
+    print("Factor Matrices created")
 
+    indexToSpaceIds = {0: user_list, 1: image_list, 2: loc_list}
+
+    def createGroups(factor_index):
+        # Create k non-overlapping groups
+        f_matrix = factors[factor_index]  # factor matrix to be used
+        groups = []
+        for i in range(k):
+            groups.append([])
+
+        for j in range(f_matrix.shape[0]):
+            # Assign object to one of k groups/latent-features that it has highest membership towards
+            object_id = indexToSpaceIds[factor_index][j] # Map indices back to user/image/location id's
+            group_index = np.argmax(f_matrix[j])
+            groups[group_index].append(object_id)
+
+        return groups
+
+    groupsList = Parallel(n_jobs=num_cores - 1)(delayed(createGroups)(i) for i in [0, 1, 2])
+
+    # Output Results
+    with open("task7_output.txt", "w") as f:
+        userGroups = groupsList[0]
+        f.write("\n********** K-USER GROUPS **********\n")
+        for i in range(k):
+            printGroups(userGroups, i, f)
+
+        imageGroups = groupsList[1]
+        f.write("\n********** K-IMAGE GROUPS **********\n")
+        for i in range(k):
+            printGroups(imageGroups, i, f)
+
+        locGroups = groupsList[2]
+        f.write("\n********** K-LOCATION GROUPS **********\n")
+        for i in range(k):
+            printGroups(locGroups, i, f)
 
 
 if __name__ == '__main__':
