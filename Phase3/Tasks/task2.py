71a9cd7b372e38e8ae499a660ab6198dd4c6daca
Akash Nigam <akashnigam2012@gmail.com>
2018-11-19 03:30:01 -0700

    added spectral partitioning

--- Phase3/Tasks/task2.py
+++ Phase3/Tasks/task2.py
@@ -1,9 +1,6 @@
 import argparse
-import numpy as np
-from sklearn.cluster import KMeans
-import multiprocessing
-
 from Phase3.APIs.visualization_apis import *
+from numpy import linalg as LA
 
 
 '''
@@ -27,10 +24,9 @@ def task2():
     # simMatrix = np.load('adjMatrix.npy')
 
     # # # Sample input # # #
-    graph = np.load('adjMatrix_new.npy')[0:200, 0:200]
+    graph = np.load('adjMatrix_new.npy')[0:20, 0:20]
     num_nodes = graph.shape[0]
-    labels = np.random.randint(10, size=num_nodes)
-    print(str(np.amax(labels)))
+    labels = spectral_partitioning(graph,4)
     visualize_groups(graph, labels, "cluster")
 
     # # # Sample input # # #
@@ -40,11 +36,76 @@ def task2():
     # mostSim.extend(['10660461066', '10754361846'])
     # visualize_similar(graph, queryImageIds, mostSim)
 
+def spectral_partitioning(graph, k):
 
-    # kmeans_model = KMeans(n_clusters=args.c, n_jobs=num_cores - 1).fit(simMatrix)
-    # kmeans = kmeans_model.transform(simMatrix)
+    num_nodes = graph.shape[0]
+    labels = np.random.randint(1, size=num_nodes)
 
+    # to make adjanceny matrix symmetric
+    for i in range(graph.shape[0]):
+        if i %1000 == 0:
+            print(i)
+        for j in range(graph.shape[1]):
+            if graph[i][j] == 1:
+                graph[j][i] = 1
 
+    clusters = [list(range(graph.shape[0]))]
+
+    for i in range(k):
+        # get largest cluster
+        tuple = max(enumerate(clusters), key=lambda x: len(x[1]))
+        max_len_cluster = tuple[1]
+        max_len_cluster_index = tuple[0]
+
+        # adjacency matrix for cluster nodes
+        cluster_adj_mat = graph[:, max_len_cluster]
+        cluster_adj_mat = cluster_adj_mat[max_len_cluster, :]
+
+        # creating new clusters from previous large cluster
+        new_clusters = get_spectral_clusters(cluster_adj_mat, max_len_cluster)
+
+        del clusters[max_len_cluster_index]
+        clusters += new_clusters
+        print(clusters)
+        print()
+
+    for i,v in enumerate(clusters):
+        labels[v] = i
+    return labels
+
+def get_spectral_clusters(adj_mat , nodeIds):
+
+    # degree of all nodes of graph
+    degrees = np.sum(adj_mat, axis=1)
+
+    # Laplacian matrix of graph
+    lap_mat = np.diag(degrees) - adj_mat
+
+    e_vals, e_vecs = LA.eig(lap_mat)
+
+    # As all eigen_values of Laplacian matrix are greater than or equal to 0 clipping any small negative value
+    e_vals = e_vals.clip(0)
+
+    sorted_evals_index = np.argsort(e_vals)
+
+    # ss_index to store index of second smallest eigen value
+    ss_index = 0
+    for index in sorted_evals_index:
+        if (e_vals[index] > 0):
+            ss_index = index
+            break
+
+    # eigen vector of second smallest eigen value
+    ss_evec = e_vecs[:, ss_index]
+
+    clusters = [[],[]]
+    for i, v in enumerate(ss_evec):
+        if v >= 0:
+            clusters[0].append(nodeIds[i])
+        else:
+            clusters[1].append(nodeIds[i])
+
+    return clusters
 
 if __name__ == '__main__':
     task2()
