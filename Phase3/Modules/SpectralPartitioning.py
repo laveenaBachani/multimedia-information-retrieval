6b16ad242113b6468837534a8f4b89e13cf311cb
Akash Nigam <akashnigam2012@gmail.com>
2018-11-25 19:33:42 -0700

    prevented 0 length cluster

--- Phase3/Modules/SpectralPartitioning.py
+++ Phase3/Modules/SpectralPartitioning.py
@@ -23,21 +23,27 @@ class SpectralPartitioning:
             old_cluster_index = 0
             # to store second smallest eigen value of old cluster which will be divided in this iteration
             old_cluster_ss_eval = sys.maxsize
-            new_clusters = clusters[old_cluster_index]
+            new_clusters = [clusters[old_cluster_index]]
+            # newclusterfound = False
             # creating new clusters from previous large cluster
             for cluster_index, cluster in enumerate(clusters):
                 # adjacency matrix for cluster nodes
+                if len(cluster) <= 1:
+                    continue
                 cluster_adj_mat = graph[:, cluster]
                 cluster_adj_mat = cluster_adj_mat[cluster, :]
                 possible_new_clusters, ss_eval = self.spectral_partition(cluster_adj_mat, cluster)
-                if ss_eval<=old_cluster_ss_eval:
+                #if ss_eval <= old_cluster_ss_eval and len(possible_new_clusters[0])>0 and len(possible_new_clusters[1])>0:
+                if ss_eval <= old_cluster_ss_eval:
                     old_cluster_ss_eval = ss_eval
                     old_cluster_index = cluster_index
                     new_clusters = possible_new_clusters
 
+
             del clusters[old_cluster_index]
             clusters += new_clusters
-            # print(clusters)
+
+            # print("newclusters:",clusters)
             # print("creating cluster ", (i + 1))
             # print()
 
@@ -61,7 +67,10 @@ class SpectralPartitioning:
 
         #print("Calculating eval and evecs")
         st = time.time()
-        e_vals, e_vecs = LA.eig(lap_mat)
+        e_vals, e_vecs = LA.eigh(lap_mat)
+        #print("lap_mat:",lap_mat)
+        #print("evals:", e_vals)
+        #print("evec:", e_vecs)
         tt = time.time() - st
         #print("Time to calculate eval of shape:", adj_mat.shape[0], " is:", tt)
 
@@ -84,14 +93,33 @@ class SpectralPartitioning:
         # second smallest eigen value
         ss_eval = e_vals[ss_index]
 
-        #print("creating 2 clus")
+        clusters = self.getClusterFromEvec(ss_evec,nodeIds)
+        #print(clusters)
+        if(len(clusters[0]) ==0 or len(clusters[1]) ==0):
+            clusters = self.getComponentsFromEvec(e_vecs[:, 1],nodeIds)
+            #print("inside:",clusters)
+            #print("evec:",e_vecs)
+        #print("recieved clus:",nodeIds)
+        #print("returing clus:",clusters)
+        return clusters, ss_eval
+
+    def getClusterFromEvec(self,ss_evec,nodeIds):
+        # print("creating 2 clus")
         clusters = [[], []]
         for i, v in enumerate(ss_evec):
             if v >= 0:
                 clusters[0].append(nodeIds[i])
             else:
                 clusters[1].append(nodeIds[i])
+        return clusters
 
-        #print("returing clus")
-        return clusters, ss_eval
+    def getComponentsFromEvec(self,ss_evec,nodeIds):
+        # print("creating 2 clus")
+        clusters = [[], []]
+        for i, v in enumerate(ss_evec):
+            if v == 0:
+                clusters[0].append(nodeIds[i])
+            else:
+                clusters[1].append(nodeIds[i])
+        return clusters
 
